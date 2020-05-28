d899cf19b430f698184fb3a33981eb15003a1d53
rafal <rafal.szostakowski@gmail.com>
2020-05-28 10:13:57 +0200

    zadanie 8 z użyciem Gherkin - klasa test

--- src/test/java/stepdefinitions/MyStoreAddDataTest.java
+++ src/test/java/stepdefinitions/MyStoreAddDataTest.java
@@ -1,12 +1,16 @@
-package junit;
+package stepdefinitions;
 
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.MyStoreAddDataPage;
+import przyklad1.AddressData;
 import przyklad1.CompanyData;
 import przyklad1.UsersData;
 
@@ -16,8 +20,9 @@ public class MyStoreAddDataTest {
 
     private WebDriver driver;
 
+// użycie metody Gherkin
 
-    @Given("an open browser with prod-kurs.coderslab.pl")
+    @Given("open browser with prod-kurs.coderslab.pl")
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 
@@ -27,17 +32,41 @@ public class MyStoreAddDataTest {
         driver.get("https://prod-kurs.coderslab.pl/index.php?controller=address");
     }
 
-    @When("user enter company data in input fileds")
-    public void companyInput(){
+    // Dodanie danych firmy z klasy CompanyData
+    @When("user enters company data in input fields")
+    public void companyInput() {
         MyStoreAddDataPage addCompanyData = new MyStoreAddDataPage(this.driver);
-        addCompanyData.addCompany(CompanyData.getRandomAlias(),CompanyData.getRadomCompany());
+        addCompanyData.addCompany(CompanyData.getRandomAlias(), CompanyData.getRadomCompany());
     }
 
-    @And("user enter user data in input fileds")
-    public void userInput(){
+    // Dodanie danych użytkownika z klasy UserData
+    @And("user enter user data in input fields")
+    public void userInput() {
         MyStoreAddDataPage addUserData = new MyStoreAddDataPage(this.driver);
-        addUserData.addUser(UsersData.getRandomMaleName(),UsersData.getRandomSurname());
+        addUserData.addUser(UsersData.getRandomMaleName(), UsersData.getRandomSurname());
     }
+
+    // Dodanie adresu z klasy AddressData
+    @And("user enter address data in input fields")
+    public void addressInput() {
+        MyStoreAddDataPage addAddressData = new MyStoreAddDataPage(this.driver);
+        addAddressData.addAddress(AddressData.getRandomAddress(), AddressData.getRandomCity(), AddressData.getRandomZip());
+    }
+
+
+    @Then("click \"Save\" to save all data")
+    public void clickSave() {
+        WebElement save = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/form/footer/button"));
+        save.click();
+    }
+
+    // sprawdzenie, czy po zapisaniu danych pojawia się informacja "Address successfully added!"
+    @And("check if all data saved")
+    public void checkSave() {
+        MyStoreAddDataPage checkSuccesfully = new MyStoreAddDataPage(this.driver);
+        Assert.assertEquals("Address successfully added!", checkSuccesfully.checkIfSave());
+    }
+
 
 }
 
